5da75bee7151c66d64be0e6e4fbf67210b95758f
dlwldke03 <jiaestelle03@gmail.com>
2026-07-19 11:53:35 +0900

    feat: 초기 세팅

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    score = models.IntegerField(default=0)
+    nickname = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        ordering = ["-score", "-date_joined"]
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def display_name(self):
+        return self.nickname or self.username
+
+    @property
+    def win_count(self):
+        return self.won_games.count()
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main(request):
+    return render(request, "core/main.html")
+
+
+def ranking(request):
+    return render(request, "core/ranking.html")
